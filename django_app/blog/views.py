690897efe8a34a99f0f09dead42e4fcd078c8339
Noah Pan <npan@MacBook-Pro.home>
2020-01-16 21:15:50 -0500

    Login.html and Logout.html added

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
+from django.contrib.auth.models import User
 
 def home(request):
 	context = { 
 		"posts" : Post.objects.all()
 	}
-
 	return render(request, 'blog/home.html', context) #context will be avalible in templates
 
 def about(request):
