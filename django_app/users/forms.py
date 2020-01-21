0bbac5cfa063152d829bfe7376649a51260db788
Noah Pan <npan@MacBook-Pro.home>
2020-01-20 20:42:54 -0500

    Profile Page added, Ability to change user details added

--- django_app/users/forms.py
+++ django_app/users/forms.py
@@ -1,11 +1,24 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
-
+#User Register Form allows user to fill out form
 class UserRegisterForm(UserCreationForm):
 	email = forms.EmailField() #default required
-
 	class Meta:
 		model = User
 		fields = ['username', 'email', 'password1', 'password2']
+
+#User Update Form allows user to update their fields
+class UserUpdateForm(forms.ModelForm):
+	email = forms.EmailField()
+	class Meta:
+		model = User
+		fields = ['username', 'email']
+
+#Dedicated Profile Form used to update the image of the profile.
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['image']
--- django_app/users/views.py
+++ django_app/users/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 # from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
 	if(request.method == 'POST'):
@@ -16,6 +17,38 @@ def register(request):
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {"form": form})
+
+#Decorator that adds functionality to the view. (This requires the user to be logged in to access the profile page)
+#More complex with class based views.
+@login_required
+def profile(request):
+	if(request.method == 'POST'):
+		u_form = UserUpdateForm(request.POST, instance= request.user)
+		p_form = ProfileUpdateForm(request.POST, 
+									request.FILES,
+								 	instance = request.user.profile)
+		#Record the previous username
+		prev_username = request.user.username
+		prev_email = request.user.email
+		if u_form.is_valid() and p_form.is_valid():
+			u_form.save()
+			p_form.save()
+			messages.success(request, f'Your account has been updated!!')
+			return redirect('profile')
+		else:
+			#Do not update template to show the modified username if it is not valid.
+			request.user.username = prev_username
+			request.user.email = prev_email
+	else:
+		u_form = UserUpdateForm(instance = request.user)
+		p_form = ProfileUpdateForm(instance = request.user.profile)
+
+	print(request.user.username)
+	context = {
+		"u_form" : u_form,
+		"p_form" : p_form 
+	}
+	return render(request, 'users/profile.html', context)
 
 
 # different message options
