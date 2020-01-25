db86ef1e8283d36828f9254fa69faa4e80b76af0
Noah Pan <npan@MacBook-Pro.home>
2020-01-25 17:10:26 -0500

    Class Based Views Added

--- django_app/blog/models.py
+++ django_app/blog/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class Post(models.Model):
 	title = models.CharField(max_length=100)
 	content = models.TextField() #unrestricted text length
 	date_posted = models.DateTimeField(default=timezone.now) #auto_now or auto_now_add also works (with slight caviates)
 	author = models.ForeignKey(User, on_delete = models.CASCADE) #on_delete-> delete post if author is deleted,
-
-
+	
+	# Defines where to redirect the user when a post is created with the PostCreateView
+	def get_absolute_url(self):
+		return reverse("post-detail", kwargs={"pk": self.pk})
+	
 	def __str__(self):
 		return self.title
 
--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -2,15 +2,79 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from blog.models import Post
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+	ListView, 
+	DetailView, 
+	CreateView,
+	UpdateView,
+	DeleteView
+)
 
+#Function based views:
+#URL Pattern is directed to certain app view. These views then handle logic for the routes.
+#Then finally they render the templates.
 def home(request):
 	context = { 
 		"posts" : Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context) #context will be avalible in templates
 
+class PostListView(ListView):
+	model = Post
+	template_name = "blog/home.html"
+	context_object_name = "posts"
+	ordering = ["-date_posted"] #order posts backwards
+	# ordering = ["date_posted"] 
+
+#Using default naming convension of the DetailView so we can cut down on code. 
+class PostDetailView(DetailView):
+	model = Post
+class PostCreateView(LoginRequiredMixin, CreateView):
+	model = Post
+	fields = ['title', 'content']
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+#LoginRequired Mixin is similar to the decorators of function based views.
+#It requires the user to be logged in to update the posts.
+#UserPassesTestMixin calls test_func, that checks requirements. 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content']
+	#Function tells who the author is of the post (who is the current user.)
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+	#test_func does not allow users to delete other peoples posts.
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model=Post
+	success_url = '/'
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+
 def about(request):
 	# context = {
 	# 	'title': 'Title of the about page'
 	# }
 	return render(request, 'blog/about.html', { 'title':'about' })
+
+
+#Class based views:
+#Have alot more built in functionality, that handles alot of back end logic.
+#Different kinds of class based views. 
+#There are list views, detail views, create views, update views, delete views.
+#Websites have similar functionality. 
+#(Blog) --> list views
+#(Youtube) --> subscription views
+#(Click on video or blog) --> detail views
+#(Ability to update or delete views) --> update/delete views.
