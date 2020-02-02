428ff5a0b37acc3cfb566b25054dbd0391dd38a9
Noah Pan <npan@MacBook-Pro.home>
2020-02-02 16:14:10 -0500

    Title modified larger

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['172.104.10.73']
+# ALLOWED_HOSTS = ['172.104.10.73']
+ALLOWED_HOSTS = []
 
 
 # Application definition
