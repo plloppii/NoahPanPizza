525b3315559acf7dd982f527870c7281751a6bb0
Noah Pan <noahpan323@gmail.com>
2021-04-10 20:43:17 -0400

    Store linter edits

--- noahpanpizza/noahpanpizza/settings.py
+++ noahpanpizza/noahpanpizza/settings.py
@@ -66,7 +66,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-SESSION_ENGINE="django.contrib.sessions.backends.cached_db"
+SESSION_ENGINE = "django.contrib.sessions.backends.cached_db"
 
 ROOT_URLCONF = 'noahpanpizza.urls'
 
--- noahpanpizza/store/views.py
+++ noahpanpizza/store/views.py
@@ -27,25 +27,36 @@ def get_current_order(request, createOrder=False):
 
     return current_order
 
-# Cart Structure: 
+# Cart Structure:
 # { str(CartID) : str(CartItem) }
 # Deserialize CartItem: line_item=next(serializers.deserialize("json", current_cart[product_id])).object
-# Serialize CartItem: current_cart[product_id] = serializers.serialize("json", [line_item])
+# Serialize CartItem: current_cart[product_id] =
+# serializers.serialize("json", [line_item])
 
-#Returns Serialized Cart Dictionary 
+# Returns Serialized Cart Dictionary
+
+
 def get_serialized_cart(request):
     if "cart" not in request.session:
-        request.session["cart"]={}
+        request.session["cart"] = {}
     return request.session["cart"]
-#Return List of CartItems
+# Return List of CartItems
+
+
 def deserialize_cart(cart):
-    return [next(serializers.deserialize("json",item)).object for item in cart.values()]
+    return [
+        next(
+            serializers.deserialize(
+                "json",
+                item)).object for item in cart.values()]
+
 
 def serialize_object(django_obj):
-    return serializers.serialize("json", [django_obj]) 
+    return serializers.serialize("json", [django_obj])
+
+
 def deserialize_object(serialized_django_obj):
     return next(serializers.deserialize("json", serialized_django_obj)).object
-
 
 
 class ProductList(ListView):
@@ -84,14 +95,15 @@ class coupon_form(View):
                 if(coupon.is_valid()):
                     response["coupon"] = coupon.coupon_code
                     response["discount"] = str(coupon.discount)
-                    self.request.session ["coupon"] = {
+                    self.request.session["coupon"] = {
                         "code": coupon.coupon_code,
                         "discount": str(coupon.discount)
                     }
-                    self.request.session.modified=True
+                    self.request.session.modified = True
                     break
-                else: 
-                    response["error"] = "Coupon Code " + str(coupon) + " is no longer valid!"
+                else:
+                    response["error"] = "Coupon Code " + \
+                        str(coupon) + " is no longer valid!"
         else:
             response["error"] = "No Valid Coupon Code: " + coupon_code
         return HttpResponse(
@@ -115,8 +127,8 @@ class contact_form(View):
                 email_address=email,
                 phone_number=phone_number
             )
-            self.request.session["contact"]= serialize_object(contact_info)
-            self.request.session.modified=True
+            self.request.session["contact"] = serialize_object(contact_info)
+            self.request.session.modified = True
             response["contact-text"] = contact_info.__str__()
             response["contact"] = json.dumps(model_to_dict(contact_info))
         else:
@@ -149,7 +161,7 @@ class shipping_form(View):
                 zipcode=zipcode
             )
 
-            billing_address=None
+            billing_address = None
             if same_billing_address:
                 billing_address = BillingAddress(
                     address1=address1,
@@ -159,9 +171,11 @@ class shipping_form(View):
                     city=city,
                     zipcode=zipcode
                 )
-                self.request.session["billing_address"] = serialize_object(billing_address)
+                self.request.session["billing_address"] = serialize_object(
+                    billing_address)
 
-            self.request.session["shipping_address"] = serialize_object(shipping_address) 
+            self.request.session["shipping_address"] = serialize_object(
+                shipping_address)
             self.request.session.modified = True
 
             response["shipping-text"] = shipping_address.__str__()
@@ -178,9 +192,11 @@ class shipping_form(View):
             content_type="application/json"
         )
 
+
 class CheckoutPage(FormView):
     form_class = CheckoutForm
     template_name = "store/checkout-page.html"
+
     def get(self, request, *args, **kwargs):
         current_cart = deserialize_cart(get_serialized_cart(request))
         if len(current_cart) != 0:
@@ -196,10 +212,10 @@ class CheckoutPage(FormView):
 class process_order(View):
     def post(self, *args, **kwargs):
         response = {}
-        
+
         # Validate Session JSON object has Shippng, Billing, CartItems, Contact
-        # Create Order, Shipping Object, Billing Address, Items, Contact, 
-        # Clear Sessions. 
+        # Create Order, Shipping Object, Billing Address, Items, Contact,
+        # Clear Sessions.
 
         for key, value in self.request.session.items():
             print('{} => {}'.format(key, value))
@@ -207,12 +223,15 @@ class process_order(View):
         # cart => {'2': '[{"model": "store.cartitem", "pk": null, "fields": {"product": 2, "quantity": 1}}]'}
         # contact => {'id': None, 'first_name': 'Noah', 'last_name': 'Pan', 'phone_number': '6463307800', 'email_address': 'noahpan323@gmail.com'}
         # billing_address => {"id": null, "address1": "202-07 56th Ave.", "address2": "", "country": "US", "state": "NY", "city": "Oakland Gardens", "zipcode": "11364"}
-        # shipping_address => {"id": null, "address1": "202-07 56th Ave.", "address2": "", "country": "US", "state": "NY", "city": "Oakland Gardens", "zipcode": "11364", "billingaddress_ptr": null}
+        # shipping_address => {"id": null, "address1": "202-07 56th Ave.",
+        # "address2": "", "country": "US", "state": "NY", "city": "Oakland
+        # Gardens", "zipcode": "11364", "billingaddress_ptr": null}
 
-        
         contact_info = deserialize_object(self.request.session["contact"])
-        shipping_info = deserialize_object(self.request.session["shipping_address"])
-        billing_info = deserialize_object(self.request.session["billing_address"])
+        shipping_info = deserialize_object(
+            self.request.session["shipping_address"])
+        billing_info = deserialize_object(
+            self.request.session["billing_address"])
 
         contact_info.save()
         shipping_info.save()
@@ -224,13 +243,13 @@ class process_order(View):
         print(shipping_info)
         print(billing_info)
 
-        newOrder= Cart(
-            contact = contact_info,
-            shipping_address = shipping_info,
-            billing_address = billing_info,
-            paypal_information = paypal_info,
-            ordered = True,
-            ordered_date = timezone.now()
+        newOrder = Cart(
+            contact=contact_info,
+            shipping_address=shipping_info,
+            billing_address=billing_info,
+            paypal_information=paypal_info,
+            ordered=True,
+            ordered_date=timezone.now()
         )
         newOrder.save()
 
@@ -245,11 +264,12 @@ class process_order(View):
 
         response["success"] = True
         response["orderid"] = newOrder.id
-            
+
         return HttpResponse(
             json.dumps(response),
             content_type="application/json"
         )
+
 
 class CheckoutSuccessPage(DetailView):
     model = Cart
@@ -271,21 +291,21 @@ def add_to_cart(request, pk, slug):
     #         cart_item.save()
     # else:
     print(current_cart)
-    #Check if item is in cart
-    #Deserialize data if item is in cart
-    #Create new object if not in cart. 
-    product_id=str(product.id)
+    # Check if item is in cart
+    # Deserialize data if item is in cart
+    # Create new object if not in cart.
+    product_id = str(product.id)
     if product_id in current_cart:
         # Deserialize the line item from sessions object
-        line_item=deserialize_object(current_cart[product_id])
-        line_item.quantity+=1
+        line_item = deserialize_object(current_cart[product_id])
+        line_item.quantity += 1
     else:
-        line_item=CartItem(product=product)
+        line_item = CartItem(product=product)
     # Reserialize the line item to be stored back into sessions
-    current_cart[product_id]=serialize_object(line_item)
+    current_cart[product_id] = serialize_object(line_item)
     request.session["cart"] = current_cart
-    request.session.modified=True
-        
+    request.session.modified = True
+
     # return HttpResponseRedirect(request.path_info)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
@@ -301,19 +321,19 @@ def delete_from_cart(request, pk, slug):
     #     cart_item, cart_item_created = CartItem.objects.get_or_create(
     #         user=request.user, product=product, order=current_order)
 
-    print("Trying to delete id: "+str(product.id))
-    product_id=str(product.id)
+    print("Trying to delete id: " + str(product.id))
+    product_id = str(product.id)
     if product_id in current_cart:
         # Deserialize the line item from sessions object
-        line_item=deserialize_object(current_cart[product_id])
+        line_item = deserialize_object(current_cart[product_id])
         if line_item.quantity <= 1:
             del current_cart[product_id]
         else:
-            line_item.quantity-=1
+            line_item.quantity -= 1
             # Reserialize the line item to be stored back into sessions
-            current_cart[product_id]=serialize_object(line_item)
+            current_cart[product_id] = serialize_object(line_item)
         request.session["cart"] = current_cart
-        request.session.modified=True
+        request.session.modified = True
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     # return HttpResponseRedirect(request.path_info)
